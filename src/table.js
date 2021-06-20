0133ccae79d705d13eaa020a624a72c9d3566110
Michelle Berta <bertafamily@sbcglobal.net>
2021-06-20 09:44:55 -0500

    initial push, create table.js and api call

---
+++ src/table.js
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+
+const Table = () => {
+const [employees, setEmployees] = useState([])
+  const getEmployees = () =>{
+    axios.get('https://randomuser.me/api/')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      setEmployees ( response.data.results )
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  
+  }
+
+const renderEmployee = (employee, index) => {
+  return(
+    <tr key={index}>
+      <td><img src={employee.picture.thumbnail}/></td>
+      <td>{`${employee.name.first} ${employee.name.last}`}</td>
+      <td>{`${employee.phone}`}</td>
+      <td>{`${employee.email}`}</td>
+      <td>{`${employee.dob.date}`}</td>
+    </tr>
+  )
+}
+useEffect(() => {
+  if (!employees) {
+    return;
+  }
+getEmployees()
+},[]) 
+  return (
+    <div className="App">
+      <ReactBootStrap.Table striped bordered hover variant="dark">
+  <thead>
+    <tr>
+      <th>Image</th>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      <th>DOB</th>
+    </tr>
+  </thead>
+  <tbody>
+    {employees.map(renderEmployee)}
+  </tbody>
+</ReactBootStrap.Table>
+    </div>
+
+   );
+}
+
+export default Table;
